abbde2ca9ec98c6d369c5a934b14b4eee33d90cc
Andre Jarboe <andrejarboe@gmail.com>
2023-01-06 15:49:11 -0500

    console log tours

--- src/App.js
+++ src/App.js
@@ -5,7 +5,33 @@ import Tours from './Tours';
 const url = 'https://course-api.com/react-tours-project';
 
 function App() {
-  return <h2>Tours Project Setup</h2>;
+	const [isLoading, setLoading] = useState(false);
+	const [tours, setTours] = useState(true);
+
+	const fetchTours = async () => {
+		setLoading(true);
+		const res = await fetch(url);
+		const tours = await res.json();
+		console.log(tours);
+    };
+    
+    useEffect(() => {
+        fetchTours();
+    }, [])
+
+	if (isLoading) {
+		return (
+			<main>
+				<Loading />
+			</main>
+		);
+	}
+
+	return (
+		<main>
+			<Tours />
+		</main>
+	);
 }
 
 export default App;
